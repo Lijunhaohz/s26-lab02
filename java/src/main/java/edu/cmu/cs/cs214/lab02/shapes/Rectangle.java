d5a60fe0b176ef11ef2566bff55d7363ba49cdb1
Junhao Li <junhaol2@andrew.cmu.edu>
2026-01-22 14:01:40 -0500

    feat: rewrite Java code

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,14 +1,15 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
+    public final double height;
+    public final double width;
     
     public Rectangle(double height, double width){
         this.height = height;
         this.width = width;
     }
 
+    @Override
     public double getArea() {
         return height * width;
     }
