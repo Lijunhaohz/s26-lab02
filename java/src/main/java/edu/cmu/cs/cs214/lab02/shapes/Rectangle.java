316f7c72a89e9b3471cd0928ca5426a192ee8ede
Junhao Li <junhaol2@andrew.cmu.edu>
2026-01-23 11:09:36 -0500

    ref: mark shape specific fields as private

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,8 +1,8 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public final double height;
-    public final double width;
+    private final double height;
+    private final double width;
     
     public Rectangle(double height, double width){
         this.height = height;
